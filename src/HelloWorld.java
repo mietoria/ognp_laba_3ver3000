9419c434ff10f7ee790b7f996314dca049bd11fd
mietoria <57088086+mietoria@users.noreply.github.com>
2021-06-02 14:15:46 +0300

    10 commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     ResourceBundle messages = ResourceBundle.getBundle("messages");
 
     public static void main(String[] args) {
-        HelloWorld he2llo = new HelloWorld();
+        HelloWorld he25llo = new HelloWorld();
         System.out.println(hello.sayHello());
     }
     String sayHello() {
