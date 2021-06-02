cf99daa099035de10dfb59c2b7306c8bc501fd1c
mietoria <sofigutara@yandex.ru>
2021-06-02 13:44:37 +0300

    4 commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     ResourceBundle messages = ResourceBundle.getBundle("messages");
 
     public static void main(String[] args) {
-        HelloWorld he54llo = new HelloWorld();
+        HelloWorld hello = new HelloWorld();
         System.out.println(hello.sayHello());
     }
     String sayHello() {
